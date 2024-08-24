a6b1e810e11b6329412ca7a731c27c054f27df20
Pa Thao <paiathao@gmail.com>
2024-08-23 23:26:46 -0500

    title by artist published in year

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -33,15 +33,21 @@ let myCollection = [];
 
   function showCollection(collection) {
     for (let i = 0; i < collection.length; i++) {
-    console.log(`${collection[i]}.title by ${collection[i]}.artist published in ${collection[i]}.yearPublished`);
+    console.log(`${collection[i].title} by ${collection[i].artist} , published in ${collection[i].yearPublished}`);
     }
   }
   showCollection(myCollection)
-//   - Create a function named `showCollection`. This function should:
-//   - Take in a `collection` parameter. (This allows it to be reused to show any array of album objects.)
-//   - Loop through the `collection` and `console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEARPUBLISHED`.
 
-// - Test the `showCollection` function.
+//   Add a function named `findByArtist`. This function should:
+//   - Take in a `collection` parameter. Remember, we want to be able to search any collection!
+//   - Take in an `artist` (string) parameter.
+//   - Create an empty array to hold any matching results, if any.
+//   - Loop through the `collection` and add any album objects with a matching artist to the array.
+//   - Return the array with the matching results. (If no results are found, an empty array should be returned.)
+
+// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
+
+// > When testing your functions, write all tests in the JavaScript file
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
