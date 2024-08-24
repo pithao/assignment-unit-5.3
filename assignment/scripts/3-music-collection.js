7ab7001a7c0cf0fac1a4932922651129f4fa729e
Pa Thao <paiathao@gmail.com>
2024-08-23 23:43:35 -0500

    add to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -31,23 +31,25 @@ let myCollection = [];
   console.log(myCollection);
   
 
-  function showCollection(collection) {
-    for (let i = 0; i < collection.length; i++) {
-    console.log(`${collection[i].title} by ${collection[i].artist} , published in ${collection[i].yearPublished}`);
-    }
+  function showCollection(collection) { //Create a function named `showCollection`. Take in a `collection` parameter
+    for (let i = 0; i < collection.length; i++) { //Loop through the `collection`
+    console.log(`${collection[i].title} by ${collection[i].artist} , published in ${collection[i].yearPublished}`); 
+    //`console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEARPUBLISHED`.
+  }
   }
   showCollection(myCollection)
 
-//   Add a function named `findByArtist`. This function should:
-//   - Take in a `collection` parameter. Remember, we want to be able to search any collection!
-//   - Take in an `artist` (string) parameter.
-//   - Create an empty array to hold any matching results, if any.
-//   - Loop through the `collection` and add any album objects with a matching artist to the array.
-//   - Return the array with the matching results. (If no results are found, an empty array should be returned.)
 
-// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
+  function findByArtist(collection, artist) {             //take in container and artist parameter
+    let matchingResults = [];                             //create an empty array to hold any matching resuts, if any
+    for (let i = 0; i < collection.length; i++) {         //Loop through the `collection` 
+      if (collection[i].artist === artist) {              //with a matching artist to the array.
+        matchingResults.push(collection[i]);              //add any album objects
+      }
+    }
+    return matchingResults                                 //Return the array with the matching results.
+  }
 
-// > When testing your functions, write all tests in the JavaScript file
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
